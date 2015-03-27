95a4d7322bc87984f7cce475df8bd2b0f168f373
Syed M <syedmuaz@gmail.com>
2015-03-27 08:51:17 +0800

    edit some code in case statements

--- FlowControl/case_statement.rb
+++ FlowControl/case_statement.rb
@@ -1,10 +1,12 @@
 a = 5
 
-case a
+answer = case a
 when 5
-  puts "a is 5"
+  "a is 5"
 when 6
-  puts "a is 6"
+  "a is 6"
 else
-  puts "a is neither 5, nor 6"
+  "a is neither 5, nor 6"
 end
+
+puts answer
